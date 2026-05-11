f7e1fe9eb784fcd839ecf251c0ff63f670c6ae48
Dina Jendeya <dinajendeya@gmail.com>
2026-05-11 20:32:27 +0300

    Remove 'apps.core' from installed apps

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.core",
     'apps.core.apps.CoreConfig',
     "apps.progress",
     "apps.blog",
