6cc9017644edd1ac7d88176f61d7dfe271b47d73
Dina Jendeya <dinajendeya@gmail.com>
2026-05-11 20:22:21 +0300

    Fix import path for CoreConfig in settings.py

--- config/settings.py
+++ config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "apps.core",
-    'core.apps.CoreConfig',
+    'apps.core.apps.CoreConfig',
     "apps.progress",
     "apps.blog",
 ]
