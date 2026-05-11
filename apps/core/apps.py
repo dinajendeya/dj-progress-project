5388109e63e5d7583609efd73ed9d1c9b2c1407f
Dina Jendeya <dinajendeya@gmail.com>
2026-05-11 20:29:18 +0300

    Refactor CoreConfig for superuser setup
    Refactor CoreConfig to improve superuser creation logic and handle exceptions.

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -1,28 +1,32 @@
 from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.core" 
+    label = "core"
 
     def ready(self):
         import os
-        from django.contrib.auth import get_user_model
+        try:
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
 
-        User = get_user_model()
+            username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+            email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
+            password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
-        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
-        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
-        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+            print("ENV USER:", username)
 
-        if username and password:
-            if not User.objects.filter(username=username).exists():
-                User.objects.create_superuser(
-                    username=username,
-                    email=email,
-                    password=password
-                )
+            if username and password:
+                if not User.objects.filter(username=username).exists():
+                    User.objects.create_superuser(
+                        username=username,
+                        email=email,
+                        password=password
+                    )
+                    print("✅ Superuser created")
+                else:
+                    print("⚠️ Superuser already exists")
 
-class CoreConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.core"
-    label = "core"
+        except Exception as e:
+            print("❌ Error:", e)
